f0295a109db47fdd3c6f4b059d06f3b60ddf0644
fefelixa <nbd23qeu@uea.ac.uk>
2025-11-12 12:37:58 +0000

    fix fps issues in video collector

--- video-collector.py
+++ video-collector.py
@@ -1,20 +1,31 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
+
 FRAMEW = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 FRAMEH = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 FPS = int(cam.get(cv2.CAP_PROP_FPS))
+cam.set(cv2.CAP_PROP_FPS, 30.0)
 print(f"video: {FRAMEW} x {FRAMEH} @ {FPS}fps")
+
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter("output.mp4", fourcc, 20.0, (FRAMEW, FRAMEH))
+out = cv2.VideoWriter("output.mp4", fourcc, FPS, (FRAMEW, FRAMEH))
+
+frame_counter = 0
+
+SECONDS = 10
 
 recording = True
 while recording:
+
     ret, frame = cam.read()
     out.write(frame)
     cv2.imshow("Camera", frame)
+    frame_counter += 1
 
-    if cv2.waitKey(1) == ord("q"):
+    # if cv2.waitKey(1) == ord("q"):
+    #     recording = False
+    if frame_counter == FPS * SECONDS:
         recording = False
 
 cam.release()
