540ad9863cb19bdd06ca9043733640399dec67ca
fefelixa <nbd23qeu@uea.ac.uk>
2025-11-19 12:55:32 +0000

    fixed video splitter to actually split videos

--- video_splitter.py
+++ video_splitter.py
@@ -1,35 +1,36 @@
 import ffmpeg, os
 import subprocess
+import librosa
 from pathlib import Path
 
 DIR = Path("long_videos/")
+
+filename = "video2.mov"
+try:
+    os.mkdir(DIR / filename[:-4])
+except:
+    pass
+# for i in range(20):
+#     outfile = filename[:-4] + f"_{i:03}.mov"
 for filename in os.listdir(DIR):
-    print(DIR / filename)
-    if filename[-4:] == ".mov":
+    if filename.endswith('.mov'):
         try:
             os.mkdir(DIR / filename[:-4])
         except:
             pass
-        # for i in range(20):
-        #     outfile = filename[:-4] + f"_{i:03}.mov"
-        command = [
-            "ffmpeg",
-            "-y",
-            "-i",
-            DIR / filename,
-            "-acodec",
-            "copy",
-            "-f",
-            "segment",
-            "-segment_time",
-            "3",
-            "-vcodec",
-            "copy",
-            "-reset_timestamps",
-            "1",
-            "-map",
-            "0",
-            DIR / filename[:-4] / "%d.mov",
-        ]
-        subprocess.run(command)
-        # input()
+        for i in range(20):
+            command2 = [
+                "ffmpeg",
+                "-ss",
+                str(i * 3),
+                "-i",
+                DIR / filename,
+                "-t",
+                "3",
+                "-c",
+                "copy",
+                DIR / filename[:-4]/f'{filename[:-4]}_{i:03}.mov',
+                '-y'
+            ]
+            subprocess.run(command2)
+# input()
